fa481345c776264a35d676e156e20d530b8b6e5d
Kevin Alfito <kevinalfito69@gmail.com>
2023-05-05 23:19:12 +0700

    Latihan Intent Camera

--- app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.dicoding.picodiploma.mycamera
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.PackageManagerCompat
+import androidx.core.content.FileProvider
+import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
 import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -61,6 +64,20 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if (it.resultCode == RESULT_OK){
+            val myFile = File(currentPhotoPath)
+            myFile.let {file ->
+                binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
+
+            }
+
+
+        }
+    }
 
     private fun uploadImage() {
         Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
@@ -71,7 +88,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startTakePhoto() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+        createTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@MainActivity,
+                "com.dicoding.picodiploma.mycamera",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
     }
 
     private fun startCameraX() {
@@ -89,7 +117,7 @@ class MainActivity : AppCompatActivity() {
                 @Suppress("DEPRECATION")
                 it.data?.getSerializableExtra("picture")
             }as? File
-            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
 
             myFile?.let {file ->
 
